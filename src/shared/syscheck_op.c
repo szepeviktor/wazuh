5d888d3c0d724a948a21933844d7daed04444c66
Gabriel Valenzuela <gabriel.valenzuela@wazuh.com>
2023-03-13 07:51:43 -0300

    Fix: Minor change on get_subkey

--- src/shared/syscheck_op.c
+++ src/shared/syscheck_op.c
@@ -1297,8 +1297,12 @@ char* get_subkey(char* key) {
         strcpy(subkey, aux_token);
         aux_token = strtok(NULL, "\\");
     }
+    int path_len = strlen(subkey) - 1;
     os_free(remaining_key);
-    if (strlen(subkey)) {
+    if (path_len) {
+        if (subkey[path_len] == '\\') {
+            subkey[path_len] = '\0';
+        }
         return subkey;
     }
     else {
