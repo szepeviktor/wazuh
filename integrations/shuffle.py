b590923d2ad49fbfef1cfea2160786ad4f772386
Gastón Palomeque <gaston.palomeque@wazuh.com>
2023-04-04 16:11:01 -0300

    Refactor integrations logging

--- integrations/shuffle.py
+++ integrations/shuffle.py
@@ -16,8 +16,10 @@
 
 import json
 import sys
-import time
+import logging
+import logging.handlers
 import os
+from urllib.parse import urlparse
 
 try:
     import requests
@@ -34,80 +36,71 @@ except ModuleNotFoundError as e:
 #      <alert_format>json</alert_format>
 #  </integration>
 
-# Global vars
-debug_enabled = False
-pwd = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-json_alert = {}
-now = time.strftime("%a %b %d %H:%M:%S %Z %Y")
-SKIP_RULE_IDS = ["87924", "87900", "87901", "87902", "87903", "87904", "86001", "86002", "86003", "87932",
-                 "80710", "87929", "87928", "5710"]
-
 # Set paths
+pwd = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 LOG_FILE = f'{pwd}/logs/integrations.log'
 
+# Global vars
+SKIP_RULE_IDS = ["87924", "87900", "87901", "87902", "87903", "87904", "86001", "86002", "86003", "87932",
+                 "80710", "87929", "87928", "5710"]
+logger = logging.getLogger("shuffle")
+consoleHandler = logging.StreamHandler()
+fileHandler = logging.handlers.WatchedFileHandler(LOG_FILE)
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s", "%a %b %d %H:%M:%S %Z %Y")
+consoleHandler.setFormatter(formatter)
+fileHandler.setFormatter(formatter)
+logger.addHandler(consoleHandler)
+logger.addHandler(fileHandler)
 
-def main(args):
-    global debug_enabled
+
+def main(args: list[str]):
     try:
         # Read arguments
         bad_arguments = False
-        if len(args) >= 4:
-            msg = '{0} {1} {2} {3} {4}'.format(
-                now,
+        if len(args) >= 3:
+            msg = '{1} {2} {3} {4}'.format(
                 args[1],
                 args[2],
                 args[3],
-                args[4] if len(args) > 4 else '',
+                args[4].upper() if len(args) > 4 else '',
             )
-            debug_enabled = (len(args) > 4 and args[4] == 'debug')
         else:
-            msg = '{0} Wrong arguments'.format(now)
             bad_arguments = True
 
-        # Logging the call
-        with open(LOG_FILE, "a") as f:
-            f.write(msg + '\n')
-
         if bad_arguments:
-            debug("# Exiting: Bad arguments. Inputted: %s" % args)
+            print_help_msg()
             sys.exit(2)
 
+        alert_file_location = args[1]
+        if not os.path.exists(alert_file_location):
+            raise FileNotFoundError(f"Alert file specified {alert_file_location} does not exist")
+
+        webhook: str = args[3]
+        if not is_valid_url(webhook):
+            raise Exception(f"Invalid webhook URL: {webhook}")
+
+        if len(args) > 4:
+            logger.setLevel(args[4].upper())
+        else:
+            logger.setLevel(logging.INFO)
+
+        # Logging the call
+        logger.debug(msg)
+
         # Main function
-        process_args(args)
+        process_args(alert_file_location, webhook)
 
     except Exception as e:
-        debug(str(e))
+        logger.error(str(e))
         raise
 
 
-def process_args(args):
-    debug("# Starting")
+def process_args(alert_file_location: str, webhook: str):
+    logger.info("Starting")
+    logger.info("Alerts file location: %s", alert_file_location)
+    logger.info("Webhook: %s", webhook)
 
-    # Read args
-    alert_file_location = args[1]
-    webhook: str = args[3]
-
-    debug("# Webhook")
-    debug(webhook)
-
-    debug("# File location")
-    debug(alert_file_location)
-
-    # Load alert. Parse JSON object.
-    try:
-        with open(alert_file_location) as alert_file:
-            json_alert = json.load(alert_file)
-    except FileNotFoundError:
-        debug("# Alert file %s doesn't exist" % alert_file_location)
-        sys.exit(3)
-    except json.decoder.JSONDecodeError as e:
-        debug("Failed getting json_alert %s" % e)
-        sys.exit(4)
-
-    debug("# Processing alert")
-    debug(json_alert)
-
-    debug("# Generating message")
+    json_alert = load_alert(alert_file_location)
     msg: str = generate_msg(json_alert)
 
     # Check if alert is skipped
@@ -115,31 +108,35 @@ def process_args(args):
         if not msg:
             return
 
-    debug(msg)
-
-    debug("# Sending message")
-
     send_msg(msg, webhook)
 
 
-def debug(msg):
-    if debug_enabled:
-        msg = "{0}: {1}\n".format(now, msg)
-        print(msg)
-        with open(LOG_FILE, "a") as f:
-            f.write(msg)
+def load_alert(file_path: str) -> object:
+    '''Load alert and parse JSON object'''
+    json_alert = {}
+    try:
+        with open(file_path, encoding='utf-8') as f:
+            json_alert = json.load(f)
+    except json.decoder.JSONDecodeError as e:
+        raise Exception(f"Failed getting json alert: {e}")
+
+    logger.info("Processing alert with ID %s", json_alert["id"])
+    logger.debug("Alert:\n%s", json_alert)
+
+    return json_alert
 
 
 # Skips container kills to stop self-recursion
-def filter_msg(alert) -> bool:
+def filter_msg(alert: object) -> bool:
     # SKIP_RULE_IDS need to be filtered because Shuffle starts Docker containers, therefore those alerts are triggered
-
     return not alert["rule"]["id"] in SKIP_RULE_IDS
 
 
-def generate_msg(alert) -> str:
+def generate_msg(alert: object) -> str:
+    logger.info("Generating message")
+
     if not filter_msg(alert):
-        print("Skipping rule %s" % alert["rule"]["id"])
+        logger.info("Skipping rule %s", alert["rule"]["id"])
         return ""
 
     level = alert['rule']['level']
@@ -158,14 +155,58 @@ def generate_msg(alert) -> str:
            'timestamp': alert["timestamp"],
            'id': alert['id'], "all_fields": alert}
 
-    return json.dumps(msg)
+    json_msg = json.dumps(msg)
+    logger.debug("Message:\n%s", json_msg)
+
+    return json_msg
 
 
 def send_msg(msg: str, url: str):
-    debug("# In send msg")
+    logger.info("Sending message")
+
     headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
     res = requests.post(url, data=msg, headers=headers, verify=False)
-    debug("# After send msg: %s" % res)
+    if res.status_code == 200:
+        logger.info("Message sent successfully")
+    else:
+        raise requests.HTTPError("Failed sending message", res.reason)
+    
+    logger.debug("Shuffle response:\nDate: %s\nStatus code: %d\nURL: %s", 
+                 res.headers["date"], res.status_code, res.url)
+
+
+def print_help_msg():
+    help_msg = '''Exiting: Invalid arguments.
+    
+Usage:
+    slack [Alerts file path] [API key] [Webhook URL] [Logging level]
+
+Arguments:
+    Alerts file path (required)
+        Path to the JSON file containing the alerts.
+
+    API key (not required)
+        The API key argument is not needed for the Shuffle integration. However, it's still considered because the 
+        integrator executes all scripts with the same arguments.
+        If you are executing the script manually, please put anything in that argument.
+
+    Webhook URL (required)
+        Slack webhook URL where the messages will be sent to.
+
+    Logging level (optional)
+        Used to define how much information should be logged. Default is INFO.
+
+        Levels: NOTSET, DEBUG, INFO, WARNING, ERROR, CRITICAL.
+    '''
+    print(help_msg)
+
+
+def is_valid_url(url: str) -> bool:
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
 
 
 if __name__ == "__main__":
