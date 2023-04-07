537fd24fccfba97031903091656e3ef2018f01ac
Julian Morales <jmorales@unc.edu.ar>
2023-04-07 19:34:53 +0000

    Adds new param to config the server

--- src/engine/source/cmds/src/defaultSettings.hpp
+++ src/engine/source/cmds/src/defaultSettings.hpp
@@ -3,38 +3,66 @@
 
 namespace cmd
 {
-// Server start
-constexpr auto ENGINE_EVENT_SOCK = "/var/ossec/queue/sockets/queue";
-constexpr auto ENGINE_EVENT_SOCK_ENV = "WZE_EVENT_SOCK";
-
-constexpr auto ENGINE_API_SOCK = "/var/ossec/queue/sockets/engine-api";
-constexpr auto ENGINE_API_SOCK_ENV = "WZE_API_SOCK";
-
-constexpr auto ENGINE_STORE_PATH = "/var/ossec/engine/store";
-constexpr auto ENGINE_STORE_PATH_ENV = "WZE_STORE_PATH";
-
-constexpr auto ENGINE_KVDB_PATH = "/var/ossec/etc/kvdb/";
-constexpr auto ENGINE_KVDB_PATH_ENV = "WZE_KVDB_PATH";
-
-constexpr auto ENGINE_ENVIRONMENT = "default:255:filter/allow-all/0:environment/wazuh/0";
-constexpr auto ENGINE_ENVIRONMENT_ENV = "WZE_ENVIRONMENT";
-
-constexpr auto ENGINE_QUEUE_SIZE = 1000000;
-constexpr auto ENGINE_QUEUE_SIZE_ENV = "WZE_QUEUE_SIZE";
-
-constexpr auto ENGINE_LOG_LEVEL = 0; // Debug
+/******************************************************************************
+ *  Server start
+ ******************************************************************************/
+// Loggin module
+constexpr auto ENGINE_LOG_LEVEL = 0;
 constexpr auto ENGINE_LOG_LEVEL_ENV = "WZE_LOG_LEVEL";
 
 constexpr auto ENGINE_LOG_OUTPUT = "/dev/stderr";
 constexpr auto ENGINE_LOG_OUTPUT_ENV = "WZE_LOG_OUTPUT";
 
-constexpr auto ENGINE_THREADS = 1;
-constexpr auto ENGINE_THREADS_ENV = "WZE_THREADS";
+// Server module
+constexpr auto ENGINE_SRV_PULL_THREADS = 1;
+constexpr auto ENGINE_SRV_PULL_THREADS_ENV = "WZE_PULL_THREADS";
 
-constexpr auto ENGINE_FLOOD_FILE = "/var/ossec/logs/engine-flood.log";
-constexpr auto ENGINE_FLOOD_FILE_ENV = "WZE_FLOOD_FILE";
+constexpr auto ENGINE_SRV_EVENT_SOCK = "/var/ossec/queue/sockets/queue";
+constexpr auto ENGINE_SRV_EVENT_SOCK_ENV = "WZE_EVENT_SOCK";
 
-// Test
+constexpr auto ENGINE_SRV_EVENT_QUEUE_TASK = 100000;
+constexpr auto ENGINE_SRV_EVENT_QUEUE_TASK_ENV = "WZE_EVENT_QUEUE_TASK";
+
+constexpr auto ENGINE_SRV_API_SOCK = "/var/ossec/queue/sockets/engine-api";
+constexpr auto ENGINE_SRV_API_SOCK_ENV = "WZE_API_SOCK";
+
+constexpr auto ENGINE_SRV_API_QUEUE_TASK = 50;
+constexpr auto ENGINE_SRV_API_QUEUE_TASK_ENV = "WZE_API_QUEUE_TASK";
+
+constexpr auto ENGINE_SRV_API_TIMEOUT = 1000;
+constexpr auto ENGINE_SRV_API_TIMEOUT_ENV = "WZE_API_TIMEOUT";
+
+// Store module
+constexpr auto ENGINE_STORE_PATH = "/var/ossec/engine/store";
+constexpr auto ENGINE_STORE_PATH_ENV = "WZE_STORE_PATH";
+
+// KVDB module
+constexpr auto ENGINE_KVDB_PATH = "/var/ossec/etc/kvdb/";
+constexpr auto ENGINE_KVDB_PATH_ENV = "WZE_KVDB_PATH";
+
+// Router module (TODO ADAPT AFTER INTEGRATION MERGE)
+constexpr auto ENGINE_ENVIRONMENT = "default:255:filter/allow-all/0:environment/wazuh/0";
+constexpr auto ENGINE_ENVIRONMENT_ENV = "WZE_ENVIRONMENT";
+
+constexpr auto ENGINE_ROUTER_THREADS = 1;
+constexpr auto ENGINE_ROUTER_THREADS_ENV = "WZE_ROUTER_THREADS";
+
+// Queue Module
+constexpr auto ENGINE_QUEUE_SIZE = 1000000;
+constexpr auto ENGINE_QUEUE_SIZE_ENV = "WZE_QUEUE_SIZE";
+
+constexpr auto ENGINE_QUEUE_FLOOD_FILE = "/var/ossec/logs/engine-flood.log";
+constexpr auto ENGINE_QUEUE_FLOOD_FILE_ENV = "WZE_QUEUE_FLOOD_FILE";
+
+constexpr auto ENGINE_QUEUE_FLOOD_ATTEMPTS = 3;
+constexpr auto ENGINE_QUEUE_FLOOD_ATTEMPTS_ENV = "WZE_QUEUE_FLOOD_ATTEMPTS";
+
+constexpr auto ENGINE_QUEUE_FLOOD_SLEEP = 100;
+constexpr auto ENGINE_QUEUE_FLOOD_SLEEP_ENV = "WZE_QUEUE_FLOOD_SLEEP";
+
+/******************************************************************************
+ *  Test
+ ******************************************************************************/
 constexpr auto ENGINE_ENVIRONMENT_TEST = "environment/wazuh/0";
 constexpr auto ENGINE_KVDB_TEST_PATH = "/var/ossec/etc/kvdb_test/";
 constexpr auto ENGINE_PROTOCOL_QUEUE = 1;
